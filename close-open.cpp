2a9605698caae7d52e86b4a08396f336ce7de0f4
unsurp4ssed <postbox6310@gmail.com>
2023-11-11 21:26:27 +0300

    -

--- close-open.cpp
+++ close-open.cpp
@@ -1,15 +1,44 @@
-#include "include/bitmapRW.h"
 #include <iostream>
-#include "include/border_checker.h"
+#include <algorithm>
+#include "bitmapRW.h"
 
 #define INPUT "../data/img3.bmp"
 #define OUTPUT "../data/img4.bmp"
 
+
+typedef enum borderType {
+    NoBorder, Up, Dwn, Left, Right
+} borderSize;
+
+typedef struct border {
+    borderSize widthBorder;
+    borderSize heightBorder;
+} border_t;
+
+border_t checkBorder(long long int i, int width, int height) {
+    borderSize widthBorder = NoBorder;
+    borderSize heightBorder = NoBorder;
+
+    long long int pxNo = i / 3;
+
+    //keeping in mind that bmp is sometimes written from down-right corner to up-left corner
+    //in that case L swaps with R amd U swaps with D, obviously
+    //TODO auto-detect how pixel data is written (UL to DR or DR to UL)
+    if (pxNo % width == 0) widthBorder = Right;
+    else if (pxNo - 1 % width == 0) widthBorder = Left;
+
+    if (pxNo <= width) heightBorder = Up;
+    else if ((pxNo <= width * height) && (pxNo >= width * (height - 1))) heightBorder = Dwn;
+
+    return { widthBorder, heightBorder };
+}
+
+
 int main()
 {
     byte* pixels; //array for data to be read from image
-    byte* pixels_filtered; //array for data to be written to
-    byte* pixels_prefiltered;
+//    byte* pixels_filtered; //array for data to be written to
+//    byte* pixels_prefiltered;
     int32_t width;
     int32_t height;
     uint32_t bytesPerPixel;
@@ -20,8 +49,8 @@ int main()
 
     ReadImage(INPUT, &pixels, &width, &height, &bytesPerPixel);
 
-    pixels_filtered = (byte*)malloc(width * height * bytesPerPixel);
-    pixels_prefiltered = (byte*)malloc(width * height * bytesPerPixel);
+    byte* pixels_filtered = (byte*)malloc(width * height * bytesPerPixel);
+    byte* pixels_prefiltered = (byte*)malloc(width * height * bytesPerPixel);
 
     for (int64_t i = 0; i < width * height * bytesPerPixel; i += bytesPerPixel)
     {
@@ -29,6 +58,14 @@ int main()
         pixels_filtered[i+1] = 0;
         pixels_filtered[i+2] = 0;
     }
+
+    for (int64_t i = 0; i < width * height * bytesPerPixel; i += bytesPerPixel)
+    {
+        pixels_prefiltered[i] = 0xff;
+        pixels_prefiltered[i + 1] = 0xff;
+        pixels_prefiltered[i + 2] = 0xff;
+    }
+
 
     for (int64_t i = 0; i < width * height * bytesPerPixel; i += bytesPerPixel) {
         int chunk[8];
@@ -42,7 +79,8 @@ int main()
         chunk[6] = ((border.heightBorder != Dwn) && (border.widthBorder != Right)) ? pixels[i + bytesPerPixel * width + bytesPerPixel] : 0;
         chunk[7] = ((border.heightBorder != Dwn) && (border.widthBorder != Left)) ? pixels[i + bytesPerPixel * width - bytesPerPixel] : 0;
         int64_t k = 0;
-        for (int64_t j = 1; j <= 8; j++)
+
+        for (int64_t j = 0; j <= 7; j++)
         {
             if (chunk[j] == 0)
             {
@@ -51,6 +89,7 @@ int main()
                 pixels_prefiltered[i + 2] = 0;
             }
         }
+
     }
 
     for (int64_t i = 0; i < width * height * bytesPerPixel; i += bytesPerPixel) {
@@ -68,13 +107,13 @@ int main()
 
                 pixels_filtered[j - bytesPerPixel * width] = 0xff;
 
-                pixels[j - bytesPerPixel * width + bytesPerPixel] = 0xff;
+                pixels_filtered[j - bytesPerPixel * width + bytesPerPixel] = 0xff;
 
-                pixels[j - bytesPerPixel * width - bytesPerPixel] = 0xff;
+                pixels_filtered[j - bytesPerPixel * width - bytesPerPixel] = 0xff;
 
-                pixels[j + bytesPerPixel * width + bytesPerPixel] = 0xff;
+                pixels_filtered[j + bytesPerPixel * width + bytesPerPixel] = 0xff;
 
-                pixels[j + bytesPerPixel * width - bytesPerPixel] = 0xff;
+                pixels_filtered[j + bytesPerPixel * width - bytesPerPixel] = 0xff;
             }
         }
     }
@@ -82,6 +121,7 @@ int main()
 
     end = clock();
     double runtime = double(end - start) / double(CLOCKS_PER_SEC);
+
 
     printf("%f s\n", runtime);
     free(pixels);
