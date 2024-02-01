c2319efd871010c5896dd50a805fdbbecaec7557
unsurp4ssed <postbox6310@gmail.com>
2024-02-01 17:41:43 +0300

    bordertype renamed

--- include/border_checker.h
+++ include/border_checker.h
@@ -7,21 +7,20 @@
 
 typedef enum borderType {
     NoBorder, Up, Dwn, Left, Right, Up2, Dwn2, Left2, Right2
-} borderSize;
+} borderType_t;
 
 typedef struct border {
-    borderSize widthBorder;
-    borderSize heightBorder;
+    borderType widthBorder;
+    borderType heightBorder;
 } border_t;
 
 border_t checkBorder(long long int i, int width, int height) {
-    borderSize widthBorder = NoBorder;
-    borderSize heightBorder = NoBorder;
+    borderType widthBorder = NoBorder;
+    borderType heightBorder = NoBorder;
 
     long long int pxNo = i / 3;
 
-    //keeping in mind that bmp is sometimes written from down-right corner to up-left corner
-    //in that case L swaps with R amd U swaps with D, obviously
+    //keeping in mind that bmp is written from down-right corner to up-left corner
     //TODO auto-detect how pixel data is written (UL to DR or DR to UL)
     if (pxNo % width == 0) widthBorder = Right;
     else if (pxNo %  2*width == 0) widthBorder = Right2;
@@ -35,6 +34,5 @@ border_t checkBorder(long long int i, int width, int height) {
 
     return { widthBorder, heightBorder };
 }
-
 
 #endif //CUBESAT_BORDER_CHECKER_H
